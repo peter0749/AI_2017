52e62c8bca456b1fd41e2746e03bc42a892a5d13
jengku <jengku@gmail.com>
2017-11-30 14:54:51 +0800

    foo

--- nqueen/main.cpp
+++ nqueen/main.cpp
@@ -3,26 +3,33 @@
 #include <functional>
 #include <cstdlib>
 #include <ctime>
+#include <sys/time.h>
 #include "Solver.hpp"
 #include "Nqueen.hpp"
 
 using namespace std;
 
 int main(int argc, char **argv) {
-    srand(time(NULL));
     if (argc<3) {
         cerr << "usage: ./" << argv[0] << "nqueen round" << endl;
         return 1;
     }
+    srand(time(NULL));
     int n_queen = atoi(argv[1]);
     int round = atoi(argv[2]);
+    struct timeval sv, ev;
+    struct timezone sz, ez;
     Nqueen nq;
     HillClimbing HC;
     GA ga(n_queen, 100, 5, 0.95, 0.95, 800, 1);
     int ga_succ=0, hc_succ=0;
     int ga_tatk=0, hc_tatk=0;
+    int accum_time_ga=0, accum_time_hc=0;
     for (int i=0; i<round; ++i) {
+        gettimeofday(&sv, &sz);
         pair<int, vector<int> > result = nq.solve(&HC, n_queen);
+        gettimeofday(&ev, &ez);
+        accum_time_hc += 1000000*(ev.tv_sec-sv.tv_sec)+(ev.tv_usec-sv.tv_usec);
         cout << "HC:" << endl;
         for (const auto v: result.second) cout << v << ' ';
         cout << "\n#attack: " << result.first << '\n' << endl;
@@ -31,18 +38,23 @@ int main(int argc, char **argv) {
 
         cout << "GA:" << endl;
         for (const auto v: result.second) cout << v << ' ';
+        gettimeofday(&sv, &sz);
         result = nq.solve(&ga, n_queen);
+        gettimeofday(&ev, &ez);
+        accum_time_ga += 1000000*(ev.tv_sec-sv.tv_sec)+(ev.tv_usec-sv.tv_usec);
         cout << "\n#attack: " << result.first << '\n' << endl;
         ga_tatk += result.first;
         ga_succ += (result.first==0);
     }
     cout << "HC:" << endl;
     cout << "average #attack: " << (float)hc_tatk/round << endl;
-    cout << "success rate: " << (float)hc_succ/round << '\n' << endl;
+    cout << "success rate: " << (float)hc_succ/round << endl;
+    cout << "average runtime: " << (float)accum_time_hc/round << " us\n" << endl;
 
     cout << "GA:" << endl;
     cout << "average #attack: " << (float)ga_tatk/round << endl;
     cout << "success rate: " << (float)ga_succ/round << endl;
+    cout << "average runtime: " << (float)accum_time_ga/round << " us" << endl;
 
     return 0;
 }
